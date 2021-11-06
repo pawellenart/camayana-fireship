dd8bb35994710792debff7aad182caa333f81416
Pawel Lenart <pawel.lenart@schange.com>
2021-11-06 10:07:24 +0100

    Project base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import lessonData from './data/lessons';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen px-8 bg-gray-900 pt-10">
+      <div className="max-w-5xl mx-auto">
+
+        {lessonData.map(lesson => (
+          <a href={lesson.url} target="_blank" rel="noreferrer">
+            <div className="h-24 flex items-center justify-center bg-gray-700 transition duration-300 hover:bg-gray-500 hover:scale-x-105">
+              <div className="text-center w-[10%] text-xl">{lesson.icon}</div>
+              <div>
+                <div className="text-white font-bold">{lesson.title}</div>
+                <div className="text-gray-300">{lesson.description}</div>
+              </div>
+              <div className="ml-auto pr-4 text-xs text-center space-y-1">
+                {lesson.free ? (
+                  <div className="p-1 w-10 text-white bg-green-400 rounded-sm">
+                    free
+                  </div>
+                ) : (
+                  ''
+                )}
+
+                <div className="p-1 w-10 text-gray-200 bg-gray-500 rounded-sm">{lesson.duration}</div>
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/data/lessons.js
@@ -0,0 +1,52 @@
+const lessonData = [
+  {
+    icon: "🔑",
+    title: "Lesson 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    url: "https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA",
+    duration: "2:12",
+    free: true
+  },
+  {
+    icon: "💾",
+    title: "Lesson 2",
+    description: "Sed id ante dignissim, dictum nibh vel, tincidunt elit.",
+    url: "https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA",
+    duration: "13:12",
+    free: true
+  },
+  {
+    icon: "📕",
+    title: "Lesson 3",
+    description: "Vestibulum ultricies sit amet lacus quis feugiat.",
+    url: "https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA",
+    duration: "5:47",
+    free: false
+  },
+  {
+    icon: "📎",
+    title: "Lesson 4",
+    description: "Nunc lobortis condimentum mauris vel fermentum.",
+    url: "https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA",
+    duration: "16:33",
+    free: false
+  },
+  {
+    icon: "🚘",
+    title: "Lesson 5",
+    description: "Ut in diam ut justo consectetur fringilla.",
+    url: "https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA",
+    duration: "8:47",
+    free: false
+  },
+  {
+    icon: "🚀",
+    title: "Lesson 6",
+    description: "Suspendisse accumsan fermentum dolor et dignissim.",
+    url: "https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA",
+    duration: "14:45",
+    free: false
+  },
+];
+
+export default lessonData;
